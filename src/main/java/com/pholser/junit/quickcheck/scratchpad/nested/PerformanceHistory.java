ffc06b61c20508cf6b815b7eb4643378968badf6
Paul Holser <pholser@alumni.rice.edu>
2018-02-02 10:19:38 -0600

    Refinement of potential 186 workaround

--- src/main/java/com/pholser/junit/quickcheck/scratchpad/nested/PerformanceHistory.java
+++ src/main/java/com/pholser/junit/quickcheck/scratchpad/nested/PerformanceHistory.java
@@ -3,13 +3,15 @@ package com.pholser.junit.quickcheck.scratchpad.nested;
 import java.time.LocalDate;
 import java.util.HashMap;
 
+import com.pholser.junit.quickcheck.generator.Size;
+
 public class PerformanceHistory {
     private final HashMap<LocalDate, Integer> h1;
     private final HashMap<LocalDate, Integer> h2;
 
     public PerformanceHistory(
-        HashMap<LocalDate, Integer> h1,
-        HashMap<LocalDate, Integer> h2) {
+        @Size(min = 1, max = 5) HashMap<LocalDate, Integer> h1,
+        @Size(min = 1, max = 5) HashMap<LocalDate, Integer> h2) {
 
         this.h1 = h1;
         this.h2 = h2;
--- src/test/java/com/pholser/junit/quickcheck/scratchpad/nested/PerformanceHistoryProperties.java
+++ src/test/java/com/pholser/junit/quickcheck/scratchpad/nested/PerformanceHistoryProperties.java
@@ -1,6 +1,8 @@
 package com.pholser.junit.quickcheck.scratchpad.nested;
 
+import com.pholser.junit.quickcheck.From;
 import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.generator.Ctor;
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
 import org.junit.runner.RunWith;
 
@@ -9,6 +11,12 @@ import static org.junit.Assert.*;
 @RunWith(JUnitQuickcheck.class)
 public class PerformanceHistoryProperties {
     @Property(trials = 10) public void notEmpty(PerformanceHistory h) {
+        assertFalse(h.empty());
+    }
+
+    @Property(trials = 10) public void alsoNotEmpty(
+        @From(Ctor.class) PerformanceHistory h) {
+
         assertFalse(h.empty());
     }
 }
