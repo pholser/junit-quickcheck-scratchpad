aaac2ce936c25584968c778670d6131fd35baa6f
Paul Holser <pholser@alumni.rice.edu>
2018-02-12 21:42:14 -0600

    Use 0.8 beta 2. Prove filtering on not-empty can advance

--- src/test/java/com/pholser/junit/quickcheck/scratchpad/nested/PerformanceHistoryGenerator.java
+++ src/test/java/com/pholser/junit/quickcheck/scratchpad/nested/PerformanceHistoryGenerator.java
@@ -3,6 +3,7 @@ package com.pholser.junit.quickcheck.scratchpad.nested;
 import java.time.LocalDate;
 import java.util.HashMap;
 
+import com.pholser.junit.quickcheck.generator.Gen;
 import com.pholser.junit.quickcheck.generator.GenerationStatus;
 import com.pholser.junit.quickcheck.generator.Generator;
 import com.pholser.junit.quickcheck.random.SourceOfRandomness;
@@ -18,20 +19,12 @@ public class PerformanceHistoryGenerator extends Generator<PerformanceHistory> {
         SourceOfRandomness random,
         GenerationStatus status) {
 
-        Generator<HashMap> maps =
-            gen().type(HashMap.class, LocalDate.class, Integer.class);
-        Generator<LocalDate> dates = gen().type(LocalDate.class);
-        Generator<Integer> ints = gen().type(Integer.class);
+        Gen<HashMap> maps =
+            gen().type(HashMap.class, LocalDate.class, Integer.class)
+                .filter(m -> !m.isEmpty());
 
-        HashMap<LocalDate, Integer> map1 = maps.generate(random, status);
-        HashMap<LocalDate, Integer> map2 = maps.generate(random, status);
-        map1.put(
-            dates.generate(random, status),
-            ints.generate(random, status));
-        map2.put(
-            dates.generate(random, status),
-            ints.generate(random, status));
-
-        return new PerformanceHistory(map1, map2);
+        return new PerformanceHistory(
+            maps.generate(random, status),
+            maps.generate(random, status));
     }
 }
